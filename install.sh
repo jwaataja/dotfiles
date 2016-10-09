9247b0896eb1871b3c168b7bf133cc9774fbd21a
Jason Waataja <jasonswaataja@gmail.com>
2016-10-09 13:40:02 -0700

    Updated some stuff

--- install.sh
+++ install.sh
@@ -4,11 +4,11 @@ echo "First install vim, emacs, redshift, and zsh. This installs files for i3
 but it doesn't need to be installed."
 
 cp .emacs ~
-mkdir ~/.i3
+mkdir -p ~/.i3
 cp .i3/config ~/.i3
 cp .i3status ~
 echo "Please reload i3."
-mkdir ~/.config
+mkdir -p ~/.config
 cp .config/redshift.conf ~/.config
 cp .jws ~
 
@@ -18,5 +18,6 @@ xrdb ~/.Xresources
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 cp .zshrc ~
 
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp .vimrc ~
 echo "Please run \"PluginInstall\" in vim."
